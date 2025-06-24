413213b3fd813a5337341adfe82ab113f6f5af03
Kostas <konstantinos.gkaris@gmail.com>
2025-06-24 14:26:05 +0200

    Get letters from letters.json:

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 session_start();
 
-$letterPoints = [
-    'A' => 1, 'B' => 3, 'C' => 3, 'D' => 2, 'E' => 1,
-    'F' => 4, 'G' => 2, 'H' => 4, 'I' => 1, 'J' => 8,
-    'K' => 5, 'L' => 1, 'M' => 3, 'N' => 1, 'O' => 1,
-    'P' => 3, 'Q' => 10, 'R' => 1, 'S' => 1, 'T' => 1,
-    'U' => 1, 'V' => 4, 'W' => 4, 'X' => 8, 'Y' => 4, 'Z' => 10
-];
+$letters = file_get_contents("./letters.json");
+$letters = json_decode($letters, true);
+
+$tileDistribution = [];
+$letterPoints = [];
+
+// Loop through letters
+foreach ($letters['letters'] as $key => $letter) {
+    $tileDistribution[$key] = $letter['tiles'];
+    $letterPoints[$key] = $letter['points'];
+}
 
 function drawTiles($num) {
     $letters = array_keys($GLOBALS['letterPoints']);
@@ -17,14 +21,6 @@ function drawTiles($num) {
     }
     return $tiles;
 }
-
-$tileDistribution = [
-    'A' => 9, 'B' => 2, 'C' => 2, 'D' => 4, 'E' => 12,
-    'F' => 2, 'G' => 3, 'H' => 2, 'I' => 9, 'J' => 1,
-    'K' => 1, 'L' => 4, 'M' => 2, 'N' => 6, 'O' => 8,
-    'P' => 2, 'Q' => 1, 'R' => 6, 'S' => 4, 'T' => 6,
-    'U' => 4, 'V' => 2, 'W' => 2, 'X' => 1, 'Y' => 2, 'Z' => 1
-];
 
 function drawTilesFromPool(&$pool, $num) {
     $drawn = [];
